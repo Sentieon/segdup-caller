7174af0f13010be8bacffecc3efc649c6e228e8e
Zhipan Li <zhipan.li@sentieon.com>
2026-03-25 13:31:26 -0700

    Fix pysam>=0.23 parse_region incompatibility (issue #3, bug 1)
    Replace pysam.AlignmentFile.parse_region() with direct string parsing to avoid breakage from pysam 0.23 changed return signature.

--- genecaller/bam_process.py
+++ genecaller/bam_process.py
@@ -422,7 +422,7 @@ class Bam:
             self.init_bamh()
         out_bamh = self.init_outbamh(out_bam)
         for region in regions.split(","):
-            _, _, start, end = self.bamh.parse_region(region=region)
+            start, end = [int(s) for s in region.split(":")[1].split("-")]
             for read in self.bamh.fetch(region=region):
                 if not read.query_sequence:
                     continue
